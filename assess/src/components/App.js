ef4b303d61dccf324c8ec90f1767108160b872fb
AldairMC <aldairmc2023@gmail.com>
2019-04-27 11:07:03 -0500

    Header & Forms finished

---
+++ assess/src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './Header/Header';
+import Form from './Form/Form'
+
+function App() {
+  return (
+    <div className="contenedor">
+      <Header 
+        title="Assess car"
+      />
+
+      <div className="contenedor-formulario">
+        <Form 
+          
+        />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
